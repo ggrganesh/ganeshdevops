714b79a106aa9ddcf17d378e45bea46c66eb4ddf
Ganesh <ganeshgr90@gmail.com>
2025-01-03 15:43:30 +0530

    variable script

---
+++ 05-array.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# author: Ganesh Reddy
+# Date: 02-01-2025
+
+MOVIES=(Salaar, Kalki, Lucky Bhasakar)
+
+# Index starts from 0 to 3 
+
+echo "First movive name is:: {$MOVIES=($0)}"
+echo "Second movive name is:: {$MOVIES=($1)}"
+echo "Third movive name is:: {$MOVIES=($2)}"
+
+# Print all movies 
+
+echo "All movies are:: {$MOVIES=(@)}"
+
