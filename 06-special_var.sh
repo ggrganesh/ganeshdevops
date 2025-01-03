a91ca5ef3d2f921533959928f823f7e6cac4092b
Ganesh <ganeshgr90@gmail.com>
2025-01-03 19:07:21 +0530

    variable script

---
+++ 06-special_var.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# author: Ganesh Reddy
+# Date: 02-01-2025
+
+echo "All veriables: $@"
+echo "Number of variables: $#"
+echo "script name: $0"
+echo "Present user: $UID"
+echo "present directory: $PWD"
+echo "home directory: $HOME"
+echo "Process id of present script: $$"
+sleep 60 & 
+echo "Process id last command background: $!"
