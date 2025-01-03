5e4ebaa86b2567b295ab2c76077d484c111e3351
Ganesh <ganeshgr90@gmail.com>
2025-01-03 14:38:29 +0530

    variable script

---
+++ variable-02.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# author: Ganesh Reddy
+# Date: 02-01-2025
+
+PERSON1=$1  # there is no space between = equalto 
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2"
+echo "$PERSON2:: Hey $PERSON1"
+echo "$PERSON1:: How are you?"
+echo "$PERSON2:: I am good how are doing?"
