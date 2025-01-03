ab129b772761ea464fc2eee6b3be7620d4c3795f
Ganesh <ganeshgr90@gmail.com>
2025-01-03 15:45:03 +0530

    variable script

--- 05-array.sh
+++ 05-array.sh
@@ -6,11 +6,11 @@ MOVIES=(Salaar, Kalki, Lucky Bhasakar)
 
 # Index starts from 0 to 3 
 
-echo "First movive name is:: {$MOVIES=($0)}"
-echo "Second movive name is:: {$MOVIES=($1)}"
-echo "Third movive name is:: {$MOVIES=($2)}"
+echo "First movive name is:: {$MOVIES=[$0]}"
+echo "Second movive name is:: {$MOVIES=[$1]}"
+echo "Third movive name is:: {$MOVIES=[$2]}"
 
 # Print all movies 
 
-echo "All movies are:: {$MOVIES=(@)}"
+echo "All movies are:: {$MOVIES=[@]}"
 
