442a996bd6afeae29432208798b7d4ff9be22bab
Ganesh <ganeshgr90@gmail.com>
2025-01-07 18:19:20 +0530

    variable script

--- 08.install.sh
+++ 08.install.sh
@@ -8,30 +8,34 @@ then
    exit 1 #other than 0
 fi
 
-dnf list installed mysql 
+dnf list installed mysql
 
 if [ $? -ne 0 ]
-then # not installed mysql start installing 
-dnf install mysql -y
-
-if [ $? -ne 0 ]
-then 
-   echo "Installing MySQL....Failure"
-   exit 1
-else 
-  echo "Insalled MySql .....Success"
+then # not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MySQL ... FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL ... SUCCESS"
+    fi
+else
+    echo "MySQL is already ... INSTALLED"
 fi
 
 dnf list installed git
 
 if [ $? -ne 0 ]
-then # not installed mysql start installing 
-dnf install git -y
-
-if [ $? -ne 0 ]
-then 
-   echo "Installing git....Failure"
-   exit 1
-else 
-  echo "Insalled git .....Success"
+then # not installed
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing GIT ... FAILURE"
+        exit 1
+    else
+        echo "Installing GIT ... SUCCESS"
+    fi
+else
+    echo "GIT is already ... INSTALLED"
 fi
