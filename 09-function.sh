e21d93c6ec6b974370de045257d9bb00677126ae
Ganesh <ganeshgr90@gmail.com>
2025-01-07 18:49:11 +0530

    variable script

--- 09-function.sh
+++ 09-function.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0];then
+if [ $USERID -ne 0 ];then
  
  echo "Error: User does'nt have sudo permission to run this script"
  exit 1  #Not other than 0 
@@ -12,10 +12,10 @@ install_package (){
   local package=$1
   echo "checking $package is insalled ..."  
   dnf list installed $package &>/dev/null
-  if ( $?-ne 0);then
+  if ( $? -ne 0);then
   echo "$package is not found installing...."
   dnf install $package -y &>/dev/null
-  if ( $?-ne 0);then
+  if ( $? -ne 0);then
   echo "Install $package failure "
   exit 1 
   else 
