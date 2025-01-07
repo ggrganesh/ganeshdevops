e9211c84538b157146c506a4810c0a2bca7351c6
Ganesh <ganeshgr90@gmail.com>
2025-01-07 18:06:02 +0530

    variable script

---
+++ 08.install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+
+USERID=$id -u
+
+if [ $USERID -ne 0 ]
+then
+echo "ERROR:: You must have sudo access to execute this script"
+exit 1 #other than 0
+fi
+
+dnf list installed mysql 
+
+if [$? -ne 0]
+then # not installed mysql start installing 
+dnf install mysql -y
+
+if [$? -ne 0 ]
+then 
+   echo "Installing MySQL....Failure"
+   exit 1
+else 
+  echo "Insalled MySql .....Success"
+fi
+
+dnf list installed git
+
+if [$? -ne 0]
+then # not installed mysql start installing 
+dnf install git -y
+
+if [$? -ne 0 ]
+then 
+   echo "Installing git....Failure"
+   exit 1
+else 
+  echo "Insalled git .....Success"
+fi
